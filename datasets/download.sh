b05fde1c3d4b3e54abf4c77fff0d6a688211c599
Octavi Obiols Sales <oobiols@login-i17.local>
2022-07-04 09:59:13 -0700

    adding link to download datasets

---
+++ datasets/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gdown https://drive.google.com/uc?id=1KDSPmw2R0ZEwJfBG-CZPAdPM21oOYDMN
+gdown https://drive.google.com/uc?id=1i_Ul07e6LDVj-N2YlbYJZ4enHOQOgugm
+gdown https://drive.google.com/uc?id=1Mm_af1CrGn1eCyf2gYHT6mCkqjY_oFcG
